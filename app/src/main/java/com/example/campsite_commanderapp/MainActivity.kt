17405e0ccabed7c80b15be02b8d3b664fd9323d1
sange371 <zusangembaleki@icloud.com>
2026-06-10 15:04:28 +0200

    Updated the Color scheme

--- app/src/main/java/com/example/campsite_commanderapp/MainActivity.kt
+++ app/src/main/java/com/example/campsite_commanderapp/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.campsite_commanderapp.ui.theme.Campsite_CommanderAppTheme
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
