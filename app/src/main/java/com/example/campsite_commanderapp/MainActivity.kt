8483e84283dcfc2460b60b81603012d9e6d45cbd
sange371 <zusangembaleki@icloud.com>
2026-06-23 23:06:30 +0200

    initial commit

--- app/src/main/java/com/example/campsite_commanderapp/MainActivity.kt
+++ app/src/main/java/com/example/campsite_commanderapp/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.MaterialTheme
 
 
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
